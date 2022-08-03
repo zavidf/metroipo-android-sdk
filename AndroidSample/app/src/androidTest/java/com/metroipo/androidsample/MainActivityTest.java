95cb41ecea2bdb99e20d92052722c597f9f40236
zavidf <91145163+zavidf@users.noreply.github.com>
2022-08-03 09:24:44 -0500

    Update MainActivityTest.java

--- AndroidSample/app/src/androidTest/java/com/metroipo/androidsample/MainActivityTest.java
+++ AndroidSample/app/src/androidTest/java/com/metroipo/androidsample/MainActivityTest.java
@@ -2,22 +2,29 @@ package com.metroipo.androidsample;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.action.ViewActions.typeTextIntoFocusedView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.metroipo.sdk.MetroIpoSdk;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class MainActivityTest {
+public class MainActivityTest extends AppCompatActivity {
+    private MetroIpoSdk metroSdk;
 
     @Test
     public void shouldDisplyWelcome(){
@@ -48,6 +55,14 @@ public class MainActivityTest {
     public void clickVerificationButton(){
         ActivityScenario scenario = ActivityScenario.launch(MainActivity.class);
         onView(withId(R.id.enter_button)).perform(click());
+    }
+    @Test
+    public void navToSignaturePad(){
+        String mCode="1111";
+        ActivityScenario scenario = ActivityScenario.launch(MainActivity.class);
+        onView(withId(R.id.codeField)).perform(typeText(mCode));
+        onView(withId(R.id.enter_button)).perform(click());
+//        onView((metroSdk.start(mCode,))).check(isDisplayed());
     }
 
 }
